59b351737d6f71060547ef3745d1170c3af76ae7
M9ICKA <m9icka13@gmail.com>
2023-12-26 01:32:49 +0300

    Homework for lecture 12

--- src/test/java/TestCasePackage/TestNG.java
+++ src/test/java/TestCasePackage/TestNG.java
@@ -1,8 +1,9 @@
+package TestCasePackage;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
